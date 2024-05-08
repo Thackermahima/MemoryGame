427b4287fbc25f5503876cb0183145c44eed1b8b
name <mahimathacker75@gmail.com>
2024-05-08 15:24:50 +0530

    Changed code to play web game in a telegram bot

--- server.js
+++ server.js
@@ -54,113 +54,128 @@ app.post('/api/move', (req, res) => {
     }
 });
 
-const token = process.env.TELEGRAM_BOT_API; // Make sure this is defined in your .env file
+// const token = process.env.TELEGRAM_BOT_API; 
 
-const bot = new TelegramBot(token, { polling: true });
+// const bot = new TelegramBot(token, { polling: true });
 
+const { Telegraf } = require("telegraf");
+const TOKEN = process.env.TELEGRAM_BOT_API;
+const bot = new Telegraf(TOKEN);
+
+const web_link = "";
+
+bot.start((ctx) =>
+  ctx.reply("Welcome..!!!!!!!!", {
+    reply_markup: {
+      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+    },
+  })
+);
+
+bot.launch();
 // Bot event handlers
-bot.onText(/\/start/, (msg) => {
-    console.log("Received /start command from user:", msg.from.id);
-    const chatId = msg.chat.id.toString();
-    let game = gameManager.initializeGame(chatId);
-    console.log(game, "game");
-    console.log(game[chatId], "game[chatId]");
-    bot.sendMessage(chatId, 'Memory Game started! Select two tiles.', {
-                reply_markup: generateKeyboard(game, chatId)
-            });
-});
+// bot.onText(/\/start/, (msg) => {
+//     console.log("Received /start command from user:", msg.from.id);
+//     const chatId = msg.chat.id.toString();
+//     let game = gameManager.initializeGame(chatId);
+//     console.log(game, "game");
+//     console.log(game[chatId], "game[chatId]");
+//     bot.sendMessage(chatId, 'Memory Game started! Select two tiles.', {
+//                 reply_markup: generateKeyboard(game, chatId)
+//             });
+// });
 
-bot.on('callback_query', (callbackQuery) => {
-    const chatId = callbackQuery.message.chat.id.toString();
-    const msg = callbackQuery.message;
-    const index = parseInt(callbackQuery.data.split('_')[1]);
+// bot.on('callback_query', (callbackQuery) => {
+//     const chatId = callbackQuery.message.chat.id.toString();
+//     const msg = callbackQuery.message;
+//     const index = parseInt(callbackQuery.data.split('_')[1]);
 
-    if (!isNaN(index)) {
-        const gameData = gameManager.makeMove(chatId, index);
+//     if (!isNaN(index)) {
+//         const gameData = gameManager.makeMove(chatId, index);
 
-        if (gameData.isGameOver) {
-             // First update the keyboard with the final matched pair
-             const keyboard = generateKeyboard(gameData, chatId);
-             bot.editMessageReplyMarkup({ inline_keyboard: keyboard.inline_keyboard }, {
-                 chat_id: chatId,
-                 message_id:  msg.message_id
-             }).then(() => {
-                 // Delay the congratulations message to let the user see the matched pair
-                 setTimeout(() => {
-                     bot.sendMessage(chatId, "Congratulations! You have matched all pairs!");
-                 }, 500);  // Adjust delay as needed
-             });
-        } else if (gameData.resetNeeded) {
-            // Update with selected tiles first
-            let tempKeyboard = generateKeyboard({ ...gameData, selections: gameData.selections }, chatId);
-            bot.editMessageReplyMarkup({ inline_keyboard: tempKeyboard.inline_keyboard }, {
-                chat_id: chatId,
-                message_id: msg.message_id
-            }).then(() => {
-                // After a brief delay, reset the view
-                                    setTimeout(() => {
-                                        gameData.selections = [];
-                                        bot.editMessageReplyMarkup({
-                                            inline_keyboard: generateKeyboard(gameData, chatId).inline_keyboard
-                                        }, {
-                                            chat_id: chatId,
-                                            message_id: msg.message_id
-                                        });
-                                    }, 300);
+//         if (gameData.isGameOver) {
+//              // First update the keyboard with the final matched pair
+//              const keyboard = generateKeyboard(gameData, chatId);
+//              bot.editMessageReplyMarkup({ inline_keyboard: keyboard.inline_keyboard }, {
+//                  chat_id: chatId,
+//                  message_id:  msg.message_id
+//              }).then(() => {
+//                  // Delay the congratulations message to let the user see the matched pair
+//                  setTimeout(() => {
+//                      bot.sendMessage(chatId, "Congratulations! You have matched all pairs!");
+//                  }, 500);  // Adjust delay as needed
+//              });
+//         } else if (gameData.resetNeeded) {
+//             // Update with selected tiles first
+//             let tempKeyboard = generateKeyboard({ ...gameData, selections: gameData.selections }, chatId);
+//             bot.editMessageReplyMarkup({ inline_keyboard: tempKeyboard.inline_keyboard }, {
+//                 chat_id: chatId,
+//                 message_id: msg.message_id
+//             }).then(() => {
+//                 // After a brief delay, reset the view
+//                                     setTimeout(() => {
+//                                         gameData.selections = [];
+//                                         bot.editMessageReplyMarkup({
+//                                             inline_keyboard: generateKeyboard(gameData, chatId).inline_keyboard
+//                                         }, {
+//                                             chat_id: chatId,
+//                                             message_id: msg.message_id
+//                                         });
+//                                     }, 300);
                                 
-            });
-        } else {
-            bot.editMessageReplyMarkup({
-                            inline_keyboard: generateKeyboard(gameData, chatId).inline_keyboard
-                        }, {
-                            chat_id: chatId,
-                            message_id: msg.message_id
-                        });
+//             });
+//         } else {
+//             bot.editMessageReplyMarkup({
+//                             inline_keyboard: generateKeyboard(gameData, chatId).inline_keyboard
+//                         }, {
+//                             chat_id: chatId,
+//                             message_id: msg.message_id
+//                         });
                     
-        }
-    } else if (callbackQuery.data === 'reset_game') {
-        const gameData = gameManager.initializeGame(chatId);  // Reset the game
-        const keyboard = generateKeyboard(gameData, chatId);
-        bot.editMessageText("Game restarted! Select two tiles.", {
-            chat_id: chatId,
-            message_id: msg.message_id,
-            reply_markup: { inline_keyboard: keyboard.inline_keyboard }
-        });
-    }
-});
+//         }
+//     } else if (callbackQuery.data === 'reset_game') {
+//         const gameData = gameManager.initializeGame(chatId);  // Reset the game
+//         const keyboard = generateKeyboard(gameData, chatId);
+//         bot.editMessageText("Game restarted! Select two tiles.", {
+//             chat_id: chatId,
+//             message_id: msg.message_id,
+//             reply_markup: { inline_keyboard: keyboard.inline_keyboard }
+//         });
+//     }
+// });
 
 
 
-function generateKeyboard(gameData, chatId) {
-    let keyboard = [];
-    let row = [];
+// function generateKeyboard(gameData, chatId) {
+//     let keyboard = [];
+//     let row = [];
 
-    gameData.board.forEach((emoji, index) => {
-        let displayEmoji = "◼️";
-        if (gameData.matchedPairs.includes(index) || gameData.selections.includes(index)) {
-            displayEmoji = emoji;
-        }
-        row.push({ text: displayEmoji, callback_data: 'select_' + index });
+//     gameData.board.forEach((emoji, index) => {
+//         let displayEmoji = "◼️";
+//         if (gameData.matchedPairs.includes(index) || gameData.selections.includes(index)) {
+//             displayEmoji = emoji;
+//         }
+//         row.push({ text: displayEmoji, callback_data: 'select_' + index });
 
-        if (row.length === 4) {
-            keyboard.push(row);
-            row = []; // Start a new row
-        }
-    });
+//         if (row.length === 4) {
+//             keyboard.push(row);
+//             row = []; // Start a new row
+//         }
+//     });
 
-    if (row.length > 0) {
-        keyboard.push(row);
-    }
+//     if (row.length > 0) {
+//         keyboard.push(row);
+//     }
 
-    // Add a reset button at the bottom
-    keyboard.push([{ text: "Reset Game", callback_data: 'reset_game' }]);
-    return { inline_keyboard: keyboard };
-}
-
-
+//     // Add a reset button at the bottom
+//     keyboard.push([{ text: "Reset Game", callback_data: 'reset_game' }]);
+//     return { inline_keyboard: keyboard };
+// }
 
 
 
-    bot.on('polling_error', (error) => {
-         console.log('Polling error:', error);
-     });
+
+
+//     bot.on('polling_error', (error) => {
+//          console.log('Polling error:', error);
+//      });
